b3b97157045361bf84684dbe4f8826b7900af65e
Bhushan Dhende <bhushandhende34@gmail.com>
2026-08-19 00:25:44 +0530

    fix: remove CREATE DATABASE statement for shared hosting compatibility

--- fitness_db.sql
+++ fitness_db.sql
@@ -7,9 +7,6 @@ SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
-CREATE DATABASE IF NOT EXISTS `fitness_db` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-USE `fitness_db`;
-
 -- --------------------------------------------------------
 -- Table structure for `users`
 -- --------------------------------------------------------
